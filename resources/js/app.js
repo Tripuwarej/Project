1924889015bb81f6c7e79b74c605821cf843dd48
nichakan <nichakan4740@gmail.com>
2023-11-06 16:05:02 +0700

    add_update_nonrole

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,23 @@
-/* import { createApp} from 'vue'
-import App from './App.vue'
+import './bootstrap';
+import '../css/app.css';
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
-const app = createApp(App)
-app.mount('#app') */
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
-import { createApp } from 'vue'; 
-
-import router from './router/index';
-
-import App from "./components/App.vue"
-
-const app = createApp(App)
-
-app.mount("#app")
-app.use(router)
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue, Ziggy)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,20 @@
-
-// vite.config.js
 import { defineConfig } from 'vite';
-
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
         vue({
             template: {
-              compilerOptions: {
-                // treat all tags with a dash as custom elements
-                isCustomElement: (tag) => tag.includes('-')
-              }
-            }
-          }),
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
         }),
     ],
 });
